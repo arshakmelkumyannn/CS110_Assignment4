de36b4a3e85c7801d38e99c641def49f21de252b
Arshak Melkumyan <arshakmelkumyan0@gmail.com>
2022-11-14 05:59:25 +0400

    comments added

--- 2_test.js
+++ 2_test.js
@@ -1,6 +1,6 @@
 const math = require("./2_mathFunctions");
-console.log(math);
-console.log(math.factorial(7));
-console.log(math.gcd(1412, 23952));
-console.log(math.hcf(14, 231));
-console.log(math.toHexadecimal(1122));
+console.log(math); //prints the object containing functions
+console.log(math.factorial(7)); //5040
+console.log(math.gcd(1412, 23952)); //4
+console.log(math.hcf(14, 231)); //7
+console.log(math.toHexadecimal(1122)); //462
