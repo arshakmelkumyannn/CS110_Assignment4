ab5ed07d1bfe473613b9118f869cd5ba94dc9360
Arshak Melkumyan <arshakmelkumyan0@gmail.com>
2022-11-15 22:40:13 +0400

    last commit

--- 1_figure.js
+++ 1_figure.js
@@ -1,13 +1,10 @@
 function printFigure(n) {
-    let space = " ";
-    let figure = "+";
-    for (let i = 1; i <= n; i++) {
-        space += space;
-        figure += figure;
-    }
+    let space = " ".repeat(n);
+    let figure = "+".repeat(n);
+
     for (let i = n; i >= 1; i--) {
         let prtLine = space.slice(0, i) + figure.slice(i - 1, n);
         console.log(prtLine);
     }
 }
-printFigure(8);
+printFigure(4);
